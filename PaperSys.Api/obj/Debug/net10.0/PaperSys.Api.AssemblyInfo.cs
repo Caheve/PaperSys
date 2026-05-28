c39955dd0808bb6c820be22abe8e31a65de8d0ba
Caheve <caheveyt@gmail.com>
2026-05-28 12:21:59 -0500

    t

--- PaperSys.Api/obj/Debug/net10.0/PaperSys.Api.AssemblyInfo.cs
+++ PaperSys.Api/obj/Debug/net10.0/PaperSys.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperSys.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64e7f046ce40eb01e02a1327e87b11bfa7f60314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a48bc10a3ebcc5f3a3a9f1c5013df64cfdad149")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperSys.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperSys.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
