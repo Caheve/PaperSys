b05ce6fdd8d95baf6afd0b6be51fc30aeaae9e55
Caheve <caheveyt@gmail.com>
2026-05-27 21:17:45 -0500

    RUUN

--- PaperSys.Api/Program.cs
+++ PaperSys.Api/Program.cs
@@ -23,14 +23,8 @@ builder.Services.AddCors(options =>
         policy =>
         {
                 policy.WithOrigins(
-                "https://papersys-app-tsd7.vercel.app",
-                "https://papersys.onrender.com",
-                "http://localhost:5173",
-                "http://localhost:5174",
-                "http://localhost:4173",
-                "http://127.0.0.1:5173",
-                "http://127.0.0.1:5174",
-                "http://127.0.0.1:4173")
+                "https://papersys.onrender.com"
+                )
 
                   .AllowAnyHeader()
                   .AllowAnyMethod();
